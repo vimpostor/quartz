dc268b288b88fedc260eb7ac94e5d733f5ed6742
Magnus Groß <magnus@mggross.com>
2024-01-04 00:57:47 +0100

    Drop flake-utils
    This site [0] was incredibly useful!
    
    [0] https://journal.platonic.systems/nix-flake-architecture-in-practice/

--- flake.nix
+++ flake.nix
@@ -2,34 +2,35 @@
 	description = "A modern Qt Quick library";
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
 	};
 
-	outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+	outputs = { self, nixpkgs }: {
+		packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
 		let
 			pkgs = nixpkgs.legacyPackages.${system};
 		in {
-			packages = rec {
-				default = pkgs.stdenv.mkDerivation {
-					pname = "quartz";
-					version = builtins.head (builtins.match ".*project\\([[:alnum:]]+ VERSION ([0-9]+\.[0-9]+).*" (builtins.readFile ./CMakeLists.txt));
+			default = pkgs.stdenv.mkDerivation {
+				pname = "quartz";
+				version = builtins.head (builtins.match ".*project\\([[:alnum:]]+ VERSION ([0-9]+\.[0-9]+).*" (builtins.readFile ./CMakeLists.txt));
 
-					src = ./.;
+				src = ./.;
 
-					nativeBuildInputs = with pkgs; [
-						cmake
-						qt6.wrapQtAppsHook
-					];
+				nativeBuildInputs = with pkgs; [
+					cmake
+					qt6.wrapQtAppsHook
+				];
 
-					buildInputs = with pkgs; [
-						qt6.qtbase
-						qt6.qtdeclarative
-					];
-				};
+				buildInputs = with pkgs; [
+					qt6.qtbase
+					qt6.qtdeclarative
+				];
 			};
-			checks = {
-				format = pkgs.runCommand "format" { src = ./.; nativeBuildInputs = [ pkgs.clang-tools pkgs.git ]; } "mkdir $out && cd $src && find . -type f -path './*\\.[hc]pp' -exec clang-format -style=file --dry-run --Werror {} \\;";
-			};
-		}
-	);
+		});
+		checks = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
+		let
+			pkgs = nixpkgs.legacyPackages.${system};
+		in {
+			format = pkgs.runCommand "format" { src = ./.; nativeBuildInputs = [ pkgs.clang-tools pkgs.git ]; } "mkdir $out && cd $src && find . -type f -path './*\\.[hc]pp' -exec clang-format -style=file --dry-run --Werror {} \\;";
+		});
+	};
 }
