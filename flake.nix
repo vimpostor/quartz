5a5884869cd51fecb453f29d820b97af2424a642
Magnus Groß <magnus@mggross.com>
2024-03-17 15:49:34 +0100

    nix: Deduplicate eachSystem

--- flake.nix
+++ flake.nix
@@ -4,33 +4,32 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 	};
 
-	outputs = { self, nixpkgs }: {
-		packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
-		let
-			pkgs = nixpkgs.legacyPackages.${system};
-		in {
-			default = pkgs.stdenv.mkDerivation {
-				pname = "quartz";
-				version = builtins.head (builtins.match ".*project\\([[:alnum:]]+ VERSION ([0-9]+\.[0-9]+).*" (builtins.readFile ./CMakeLists.txt));
+	outputs = { self, nixpkgs }:
+	let eachSystem = with nixpkgs.lib; f: foldAttrs mergeAttrs {} (map (s: mapAttrs (_: v: { ${s} = v; }) (f s)) systems.flakeExposed);
+	in eachSystem (system:
+		let pkgs = nixpkgs.legacyPackages.${system}; in
+		{
+			packages = {
+				default = pkgs.stdenv.mkDerivation {
+					pname = "quartz";
+					version = builtins.head (builtins.match ".*project\\([[:alnum:]]+ VERSION ([0-9]+\.[0-9]+).*" (builtins.readFile ./CMakeLists.txt));
 
-				src = ./.;
+					src = ./.;
 
-				nativeBuildInputs = with pkgs; [
-					cmake
-					qt6.wrapQtAppsHook
-				];
+					nativeBuildInputs = with pkgs; [
+						cmake
+						qt6.wrapQtAppsHook
+					];
 
-				buildInputs = with pkgs; [
-					qt6.qtbase
-					qt6.qtdeclarative
-				];
+					buildInputs = with pkgs; [
+						qt6.qtbase
+						qt6.qtdeclarative
+					];
+				};
 			};
-		});
-		checks = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
-		let
-			pkgs = nixpkgs.legacyPackages.${system};
-		in {
-			format = pkgs.runCommand "format" { src = ./.; nativeBuildInputs = [ pkgs.clang-tools pkgs.git ]; } "mkdir $out && cd $src && find . -type f -path './*\\.[hc]pp' -exec clang-format -style=file --dry-run --Werror {} \\;";
-		});
-	};
+			checks = {
+				format = pkgs.runCommand "format" { src = ./.; nativeBuildInputs = [ pkgs.clang-tools pkgs.git ]; } "mkdir $out && cd $src && find . -type f -path './*\\.[hc]pp' -exec clang-format -style=file --dry-run --Werror {} \\;";
+			};
+		}
+	);
 }
